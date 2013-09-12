9164eca60caf258234e52c1040fa172abc9a6dec
lambeletjp <jp_lambelet_@hotmail.com>
2013-09-12 23:44:25 +0200

    Commit to Github

---
+++ src/main/java/gasstation/newGasStation.java
@@ -0,0 +1,249 @@
+package gasstation;
+
+import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import net.bigpoint.assessment.gasstation.GasPump;
+import net.bigpoint.assessment.gasstation.GasStation;
+import net.bigpoint.assessment.gasstation.GasType;
+import net.bigpoint.assessment.gasstation.exceptions.GasTooExpensiveException;
+import net.bigpoint.assessment.gasstation.exceptions.NotEnoughGasException;
+
+/**
+ * Implementation of a GasStation. It's thread safe.
+ * @author jp
+ * @version 0.1
+ * @date 2013-09-12
+ */
+public class newGasStation implements GasStation {
+
+    private List<GasPump> arrayGasPumps = new ArrayList(); //the array where alle the gas pump will be saved
+    private List<Integer> arrayFreeGasPump = new ArrayList(); //the array to set which pump is not used
+    private double revenue = 0; //the revenue total of the gas station
+    private int numberOfSales = 0; //the number of sales
+    private int numberOfCancellationsNoGas = 0; //the number of cancellation if no Gas
+    private int numberOfCancellationsTooExpensive = 0; //the number of cancellation if too expensive
+    private Map hTablePriceOfGas = Collections.synchronizedMap(new HashMap()); //the price of the gaz we had
+    private final int numberMaxWaitPump = 50; //how many time are we going to try to get a pump
+
+    //default constructeur
+    public newGasStation() {
+        //set price of all type of gas of max int
+        for (GasType gasType : GasType.values()) {
+            this.setPrice(gasType, Double.MAX_VALUE);
+        }
+    }
+
+    /**
+     * Add a gas pump to this station.
+     * This is used to set up this station.
+     * 
+     * @param pump
+     *            the gas pump
+     */
+    public void addGasPump(GasPump pump) {
+        synchronized (this.arrayGasPumps) {
+            arrayGasPumps.add(pump);
+            arrayFreeGasPump.add(1);
+        }
+    }
+
+    /**
+     * Get all gas pumps that are currently associated with this gas station.
+     * 
+     * Modifying the resulting collection should not affect this gas station.
+     * 
+     * @return A collection of all gas pumps.
+     */
+    public Collection<GasPump> getGasPumps() {
+        return new ArrayList(this.arrayGasPumps);
+    }
+
+    /**
+     * Simulates a customer wanting to buy a specific amount of gas.
+     * 
+     * @param type
+     *            The type of gas the customer wants to buy
+     * @param amountInLiters
+     *            The amount of gas the customer wants to buy. Nothing less than this amount is acceptable!
+     * @param maxPricePerLiter
+     *            The maximum price the customer is willing to pay per liter
+     * @return the price the customer has to pay for this transaction
+     * @throws NotEnoughGasException
+     *             Should be thrown in case not enough gas of this type can be provided
+     *             by any single {@link GasPump}.
+     * @throws GasTooExpensiveException
+     *             Should be thrown if gas is not sold at the requested price (or any lower price)
+     */
+    public double buyGas(GasType type, double amountInLiters, double maxPricePerLiter) throws NotEnoughGasException, GasTooExpensiveException {
+        //is the price low enough?
+        double price = this.getPrice(type) * amountInLiters;
+        if (this.getPrice(type) > maxPricePerLiter) {
+            this.addNumberOfCancellationsTooExpensive();
+            throw new GasTooExpensiveException();
+        }
+
+        //find the right pump
+        List arrayChecked = new ArrayList(); //list to add which pump has been checked
+        int numberWaitPump = 0; //how many time we will wait to get a Pump
+        GasPump gasPump = null; //will be the right pump
+        int i = -1;
+        while (this.arrayFreeGasPump.size() != arrayChecked.size()) {
+            i++;
+
+            //if we already checked this pump we don't want to do it again immediately, so we wait to check again
+            if (Arrays.asList(arrayChecked).contains(i)) {
+                //check if we already waited too many time
+                numberWaitPump++;
+                if (numberWaitPump > this.numberMaxWaitPump) {
+                    break;
+                }
+                try {
+                    Thread.sleep((long) (100));
+                } catch (InterruptedException ex) {
+                    //ignored
+                }
+                continue;
+            }
+
+            //is the gasPump free?
+            synchronized (this.arrayFreeGasPump.get(i)) {
+                if (this.arrayFreeGasPump.get(i) == 0) {
+                    continue;
+                }
+            }
+            synchronized (this.arrayGasPumps.get(i)) {
+                gasPump = this.arrayGasPumps.get(i);
+                arrayChecked.add(i);
+
+                //has the gasPump the right gasType?
+                if (gasPump.getGasType() != type) {
+                    gasPump = null;
+                    continue;
+                }
+
+                //is there enough gas?
+                if (gasPump.getRemainingAmount() < amountInLiters) {
+                    gasPump = null;
+                    continue;
+                }
+
+                //we take this one!
+                synchronized (this.arrayFreeGasPump) {
+                    this.arrayFreeGasPump.set(i, 0);
+                }
+                break;
+            }
+        }
+
+        /* 
+         * If we didn't find a gasPump it's because:
+         * 1) no pump with this type of gas
+         * 2) there's no enough Gas
+         * 3) we wait too long to get the gas
+         */
+        if (gasPump == null) {
+            this.addNumberOfCancellationsNoGas();
+            throw new NotEnoughGasException();
+        }
+
+        //pump the gas
+        synchronized (gasPump) {
+            this.addRevenue(price);
+            this.addNumberOfSales();
+            gasPump.pumpGas(amountInLiters);
+        }
+
+        //set the pump free again
+        synchronized (this.arrayFreeGasPump) {
+            this.arrayFreeGasPump.set(i, 1);
+        }
+
+        return price;
+    }
+    /**
+     * Used internal to add a a Revenue
+     */
+    private synchronized void addRevenue(double revenue) {
+        this.revenue += revenue;
+    }
+    /**
+     * @return the total revenue generated
+     */
+    public double getRevenue() {
+        return this.revenue;
+    }
+
+    /**
+     * Used internal to add a a Sale
+     */
+    private synchronized void addNumberOfSales() {
+        this.numberOfSales++;
+    }
+
+    /**
+     * Returns the number of successful sales. This should not include cancelled sales.
+     * 
+     * @return the number of sales that were successful
+     */
+    public int getNumberOfSales() {
+        return this.numberOfSales;
+    }
+
+    /**
+     * Used internal to add a Cancellation because there were no gas anymore
+     */
+    private synchronized void addNumberOfCancellationsNoGas() {
+        this.numberOfCancellationsNoGas++;
+    }
+
+    /**
+     * @return the number of cancelled transactions due to not enough gas being available
+     */
+    public int getNumberOfCancellationsNoGas() {
+        return this.numberOfCancellationsNoGas;
+    }
+
+    /**
+     * Used internal to add a Cancellation because it was too expensive
+     */
+    private synchronized void addNumberOfCancellationsTooExpensive() {
+        this.numberOfCancellationsTooExpensive++;
+    }
+
+    /**
+     * Returns the number of cancelled transactions due to the gas being more expensive than what the customer wanted to pay
+     * 
+     * @return the number of cancelled transactions
+     */
+    public int getNumberOfCancellationsTooExpensive() {
+        return this.numberOfCancellationsTooExpensive;
+    }
+
+    /**
+     * Get the price for a specific type of gas
+     * 
+     * @param type
+     *            the type of gas
+     * @return the price per liter for this type of gas
+     */
+    public double getPrice(GasType type) {
+        return (Double) this.hTablePriceOfGas.get(type);
+    }
+
+    /**
+     * Set a new price for a specific type of gas
+     * 
+     * @param type
+     *            the type of gas
+     * @param price
+     *            the new price per liter for this type of gas
+     */
+    public void setPrice(GasType type, double price) {
+        this.hTablePriceOfGas.put(type, price);
+    }
+}
---
+++ src/test/java/gasstation/newGasStationTest.java
@@ -0,0 +1,182 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gasstation;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import net.bigpoint.assessment.gasstation.GasPump;
+import net.bigpoint.assessment.gasstation.GasType;
+import net.bigpoint.assessment.gasstation.exceptions.GasTooExpensiveException;
+import net.bigpoint.assessment.gasstation.exceptions.NotEnoughGasException;
+import org.junit.AfterClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author JP
+ */
+public class newGasStationTest {
+    
+    public newGasStationTest() {
+    }
+
+    /**
+     * Test of addGasPump method, of class newGasStation.
+     */
+    @org.junit.Test
+    public void testAddAndGetGasPump() {
+        System.out.println("addGasPump");
+        GasPump pump = new GasPump(GasType.DIESEL,10);
+        newGasStation instance = new newGasStation();
+        instance.addGasPump(pump);
+        List listGasPump = (List) instance.getGasPumps();
+        if(listGasPump.size() != 1){
+            fail("Failed to add a pump");
+        }
+        if(listGasPump.get(0) != pump){
+            fail("Failed to get the same pump");
+        }
+    }
+
+    /**
+     * Test of buyGas method, of class newGasStation.
+     */
+    @org.junit.Test
+    public void testBuyGas() throws Exception {
+        System.out.println("buyGas");
+        GasType type = GasType.DIESEL;
+        double amountInLiters = 10.0;
+        double maxPricePerLiter = 10.0;        
+        newGasStation gasStation = new newGasStation();
+        gasStation.setPrice(GasType.DIESEL, 10);
+        GasPump pump = new GasPump(GasType.DIESEL,10);
+        gasStation.addGasPump(pump);
+        double expResult = 100.0;
+        double result = gasStation.buyGas(type, amountInLiters, maxPricePerLiter);
+        assertEquals(expResult, result, 0.0);
+    }
+
+    /**
+     * Test of getRevenue method, of class newGasStation.
+     */
+    @org.junit.Test
+    public void testGetRevenue() {
+        System.out.println("testGetRevenue");
+        GasType type = GasType.DIESEL;
+        double amountInLiters = 10.0;
+        double maxPricePerLiter = 10.0;        
+        newGasStation gasStation = new newGasStation();
+        gasStation.setPrice(GasType.DIESEL, 10);
+        GasPump pump = new GasPump(GasType.DIESEL,10);
+        gasStation.addGasPump(pump);
+        double expResult = 100.0;
+        try {
+            gasStation.buyGas(type, amountInLiters, maxPricePerLiter);
+        } catch (NotEnoughGasException ex) {
+            Logger.getLogger(newGasStationTest.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (GasTooExpensiveException ex) {
+            Logger.getLogger(newGasStationTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        double result = gasStation.getRevenue();
+        assertEquals(expResult, result, 0.0);
+    }
+
+    /**
+     * Test of getNumberOfSales method, of class newGasStation.
+     */
+    @org.junit.Test
+    public void testGetNumberOfSales() {
+        System.out.println("testGetNumberOfSales");
+        GasType type = GasType.DIESEL;
+        double amountInLiters = 10.0;
+        double maxPricePerLiter = 10.0;        
+        newGasStation gasStation = new newGasStation();
+        gasStation.setPrice(GasType.DIESEL, 10);
+        GasPump pump = new GasPump(GasType.DIESEL,10);
+        gasStation.addGasPump(pump);
+        int expResult = 1;
+        try {
+            gasStation.buyGas(type, amountInLiters, maxPricePerLiter);
+        } catch (NotEnoughGasException ex) {
+            Logger.getLogger(newGasStationTest.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (GasTooExpensiveException ex) {
+            Logger.getLogger(newGasStationTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        double result = gasStation.getNumberOfSales();
+        assertEquals(expResult, result, 0.0);
+    }
+
+    /**
+     * Test of getNumberOfCancellationsNoGas method, of class newGasStation.
+     */
+    @org.junit.Test
+    public void testGetNumberOfCancellationsNoGas() {
+        System.out.println("testGetNumberOfCancellationsNoGas");
+        GasType type = GasType.DIESEL;
+        double amountInLiters = 10.0;
+        double maxPricePerLiter = 10.0;        
+        newGasStation gasStation = new newGasStation();
+        gasStation.setPrice(GasType.DIESEL, 10);
+        GasPump pump = new GasPump(GasType.DIESEL,1);
+        gasStation.addGasPump(pump);
+        int expResult = 1;
+        try {
+            gasStation.buyGas(type, amountInLiters, maxPricePerLiter);
+            System.out.println("+++++++++++++++++++++++++++++++");
+        } catch (NotEnoughGasException ex) {
+            //success
+        } catch (GasTooExpensiveException ex) {
+            Logger.getLogger(newGasStationTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        finally{
+            double result = gasStation.getNumberOfCancellationsNoGas();
+            assertEquals(expResult, result, 0.0);  
+        }
+        
+    }
+
+    /**
+     * Test of getNumberOfCancellationsTooExpensive method, of class newGasStation.
+     */
+    @org.junit.Test
+    public void testGetNumberOfCancellationsTooExpensive() {
+        System.out.println("testGetNumberOfCancellationsTooExpensive");
+        GasType type = GasType.DIESEL;
+        double amountInLiters = 10.0;
+        double maxPricePerLiter = 1.0;        
+        newGasStation gasStation = new newGasStation();
+        gasStation.setPrice(GasType.DIESEL, 10);
+        GasPump pump = new GasPump(GasType.DIESEL,10);
+        gasStation.addGasPump(pump);
+        try {
+            gasStation.buyGas(type, amountInLiters, maxPricePerLiter);
+        } catch (NotEnoughGasException ex) {
+            fail("The GasTooExpensiveException was not thrown");
+        } catch (GasTooExpensiveException ex) {
+            return;
+        } 
+        fail("The GasTooExpensiveException was not thrown");
+        
+    }
+
+    /**
+     * Test of getPrice method, of class newGasStation.
+     */
+    @org.junit.Test
+    public void testSetGetPrice() {
+        System.out.println("getPrice");
+        GasType type = GasType.DIESEL;
+        newGasStation gasStation = new newGasStation();
+        gasStation.setPrice(type, 10);
+        double expResult = 10.0;
+        double result = gasStation.getPrice(type);
+        assertEquals(expResult, result, 0.0);
+    }
+}
